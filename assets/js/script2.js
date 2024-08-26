58e143d5a468106759ae7943034439f442d04982
JRejas <rejas.jose@gmail.com>
2024-08-26 19:23:07 -0400

    se arreglo script y formulario de contacto

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,6 +1,22 @@
+
 $(document).ready(function () {
+  
+    
     $('#btnEnviar').click(function () { 
-        alert("Datos enviados satisfactoriamente");
+        const nombre = $("#nombre").val();    
+        const apellido = $("#apellido").val();    
+        const telefono = $("#telefono").val();    
+        const correo = $("#correo").val();    
+   /*      console.log(nombre);
+        console.log(apellido);
+        console.log(telefono);
+        console.log(correo); */
+        if(nombre == "" || apellido == "" || telefono == "" || correo ==""){
+            alert("Por favor, complete todos los campos");
+        } else{
+            alert("Datos enviados satisfactoriamente");
+        }
+        
         
     });    
 });
